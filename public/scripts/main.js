5c79836212c1652d41398ad0d00ecf8ec5c3e955
rhit-jinm <jinm@rose-hulman.edu>
2021-10-18 01:32:33 -0400

    finished login sign up homepage(need to init fb)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -31,6 +31,22 @@ rhit.ClassName = class {
 /** function and class syntax examples */
 rhit.main = function () {
 	console.log("Ready");
+	rhit.startFirebaseUI();
 };
 
+rhit.startFirebaseUI= function() {
+	var uiConfig = {
+        signInSuccessUrl: '/',
+        signInOptions: [
+          // Leave the lines as is for the providers you want to offer your users.
+          firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+          firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        ],
+       
+      };
+
+      var ui = new firebaseui.auth.AuthUI(firebase.auth());
+     
+      ui.start('#firebaseui-auth-container', uiConfig);
+}
 rhit.main();
